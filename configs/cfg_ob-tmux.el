2709e2b7feb61dbab2d9c5926fa4b98ade7a3bbd
Alex Koval <alex.v.koval@labster.com>
2019-06-07 07:33:22 +0300

    sync with hg

--- configs/cfg_ob-tmux.el
+++ configs/cfg_ob-tmux.el
@@ -5,7 +5,7 @@
     (:socket  . nil)              ; The default tmux socket to communicate with
     ;; You can use "xterm" and "gnome-terminal".
     ;; On mac, you can use "iterm" as well.
-    (:terminal . "urxvt")))
+    (:terminal . "xterm")))
 
 ;; The tmux sessions are prefixed with the following string.
 ;; You can customize this if you like.
--- init.el
+++ init.el
@@ -16,11 +16,6 @@
  ;; If there is more than one, they won't work right.
  '(helm-firefox-show-structure t t)
  '(helm-idle-delay 0.2 t)
+ '(markdown-command "/usr/bin/pandoc")
  '(package-selected-packages '(dash auto-compile use-package))
  '(uniquify-buffer-name-style 'post-forward nil (uniquify)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
